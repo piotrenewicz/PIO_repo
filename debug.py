7eec15da587359f7e314e2a4dfac7960e8c2f7f0
Piotrenewicz <piotrekm9@onet.eu>
2020-05-22 19:37:48 +0200

    better debugging setup, main is ready to be cleared for GUI

---
+++ debug.py
@@ -0,0 +1,35 @@
+import data_processing
+import fdb
+
+
+def run():
+    connection_args = data_processing.read_config("connection_config.txt")
+    query = data_processing.render_query("01.01.2020")
+
+    # query = "SELECT * FROM VIEW_OKNO_ZAKU;"
+    # query = "select * from VIEW_OKNO_FAKT;"
+    # query = "SELECT RDB$RELATION_NAME, RDB$DESCRIPTION  FROM RDB$RELATIONS;"
+
+    # header, data = data_processing.read_database(connection_args, query)
+
+    con = fdb.connect(**connection_args)
+    cur = con.cursor()
+    cur.execute(query)
+
+    # Print a header.
+    for fieldDesc in cur.description:
+        print(fieldDesc[fdb.DESCRIPTION_NAME].ljust(fieldDesc[fdb.DESCRIPTION_DISPLAY_SIZE]), end=" ")
+    print()  # Finish the header with a newline.
+    print('-' * 78)
+
+    # For each row, print the value of each field left-justified within
+    # the maximum possible width of that field.
+    fieldIndices = range(len(cur.description))
+    for row in cur:
+        for fieldIndex in fieldIndices:
+            fieldValue = str(row[fieldIndex])
+            fieldMaxWidth = cur.description[fieldIndex][fdb.DESCRIPTION_DISPLAY_SIZE]
+
+            print(fieldValue.ljust(fieldMaxWidth), end=" ")
+
+        print()  # Finish the row with a newline.
--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import tkinter as tk
 import data_processing
 
 
-# data_processing.execute()
-# exit()  # comment this to have the code below run
+data_processing.execute()
+exit()  # comment this for debug
+import debug
+debug.run()
+exit()
 
+
+# GOING FURTHER DOWN IS NOW deprecated
+print("this shouldn't execute")
+# soon to be removed, and replaced with debug file
+# if there's anything of value left here copy it to debug
 
 connect_args = {}  # TODO: obsługa braku pliku. W takim przypadku otworzyć ustawienia, zebrać dane, i stworzyć ten plik
 with open("connection_config.txt", "r") as f:
