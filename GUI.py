fd0de73fb0db78ea3d36de978b1abaf943f3d419
Piotrenewicz <piotrekm9@onet.eu>
2020-05-23 14:08:16 +0200

    Super Crude GUI design

--- GUI.py
+++ GUI.py
@@ -4,6 +4,21 @@ from PIL import ImageTk, Image
 
 def settings():
     settings_window = Tk()
+    # ======= Ustawienia Połączenia ==================
+    # hasło użytkownika SYSDBA
+    # IP servera bazy danych
+    # Numer Portu
+    # Katalog Główny programu FAKT
+    # [Sprawdź Połączenie]
+    # ================================================
+
+    # ======= Inne Ustawienia ========================
+    # Ścieżka / Nazwa Pliku docelowego
+    # ?????? Cokolwiek jeszcze będziemy potrzebować
+    # Jeśli się będziemy bawić w kolory to możemy tu to ustawiać
+    # Ale wątpie
+    #
+    # [Anuluj?]  [Zapisz]
 
 
     settings_window.mainloop()
@@ -14,6 +29,19 @@ settings()
 
 def lobby():
     main_window = Tk()
+    # ======= Podziel Spóźnienia na =============
+    # [-] [0            ]
+    # [-] [30           ]
+    # [-] [60           ]
+    # [-] [90           ]
+    # [-] [180          ]
+    # [+]
+    #
+    # ID Firmy do zestawienia: [1   ]           # potem trzeba będzie zrobić padding    [1   ] == [0001]
+    # [Zestawienie Sprzedaży]
+    # [Zestawienie Zakupów  ]
+    #
+    # [Ustawienia]      [Zamknij Program]
 
 
     main_window.mainloop()
